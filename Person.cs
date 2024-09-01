49ff98ee9afb5a2834dd05a5285745aa89cd8190
TruongHongAnh2004 <honganhtruong46@gmail.com>
2024-09-01 17:21:35 +0700

    second

---
+++ Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HocCS
+{
+    internal class Person
+    {
+
+        public string Name { get; set; }
+        public float MathScore { get; set; }
+        public float EnglishScore { get; set; }
+        public float ScienceScore { get; set; }
+
+        public float AverageScore()
+        {
+            return (MathScore + EnglishScore + ScienceScore) / 3.0f;
+        }
+
+        public string GetClassification()
+        {
+            float average = AverageScore();
+            if (average >= 8.5)
+            {
+                return "Giỏi.";
+            }
+            else if (average >= 7.0)
+            {
+                return "Khá.";
+            }
+            else if (average >= 5.0)
+            {
+                return "Trung Bình.";
+            }
+            else
+            {
+                return "Yếu.";
+            }
+        }
+
+
+        public static float GetScore(string subject, string name)
+        {
+            float score;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Nhập điểm {subject} của {name} là: ");
+                    score = float.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Lỗi: Vui lòng nhập điểm dưới dạng số.");
+                }
+            }
+            return score;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,78 +1,37 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
+using HocCS;
 
-namespace BaiTap
+
+
+
+Console.OutputEncoding = Encoding.UTF8;
+
+var students = new Person();
+
+Console.Write("Nhập số lượng học sinh: ");
+int numOfStudents = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numOfStudents; i++)
 {
+    Console.WriteLine("Nhập thông tin học sinh thứ "+ i+1 +" :");
 
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.OutputEncoding = Encoding.UTF8;
+    Person student = new Person();
 
-            //Câu 1
-            Console.Write("Nhập họ tên học sinh: ");
-            string name = Console.ReadLine();
+    Console.Write("Nhập họ tên học sinh: ");
+    student.Name = Console.ReadLine();
 
-            Console.Write("Nhập điểm Toán của " + name + " là: ");
-            float mathScore = float.Parse(Console.ReadLine());
+    student.MathScore = Person.GetScore("Toán", student.Name);
+    student.EnglishScore = Person.GetScore("Tiếng Anh", student.Name);
+    student.ScienceScore = Person.GetScore("Khoa học", student.Name);
+}
 
-            Console.Write("Nhập điểm Tiếng Anh của " + name + " là: ");
-            float englishScore = float.Parse(Console.ReadLine());
-
-            Console.Write("Nhập điểm Khoa học của " + name + " là: ");
-            float scienceScore = float.Parse(Console.ReadLine());
-
-            //Câu 2
-            float averageScore = (mathScore + englishScore + scienceScore) / 3.0f;
-
-            Console.WriteLine("Điểm Trung Bình của " + name + " là: " + averageScore);
-
-            Console.WriteLine("Họ và tên: " + name);
-            Console.WriteLine("Điểm các môn lần lượt là: "+ mathScore + " "+ englishScore + " "+ scienceScore);
-            Console.WriteLine("DTB: " + averageScore);
-            //Câu 3
-            Console.Write("Xếp loại: ");
-            if (averageScore >= 8.5)
-            {
-                Console.WriteLine("Giỏi.");
-            }
-            else
-            {
-                if (averageScore <= 8.4 && averageScore >= 7.0)
-                {
-                    Console.WriteLine("Khá.");
-                }
-                else if (averageScore <= 6.9 && averageScore >= 5.0)
-                {
-                    Console.WriteLine("Trung Bình.");
-                }
-                else
-                {
-                    Console.WriteLine("Yếu.");
-                }
-            }
-            static float GetScore(string subject, string name)
-            {
-                float score;
-                while (true)
-                {
-                    try
-                    {
-                        Console.Write($"Nhập điểm {subject} của {name} là: ");
-                        score = float.Parse(Console.ReadLine());
-                        break;
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Lỗi: Vui lòng nhập điểm dưới dạng số.");
-                    }
-                }
-                return score;
-            }
-
-        }
-
-    }
-
+Console.WriteLine("Danh sách học sinh đã nhập: ");
+for (int i = 0; i < numOfStudents; i++)
+{
+    Console.WriteLine("Họ và tên: " + student.Name);
+    Console.WriteLine("Điểm Toán: " + student.MathScore);
+    Console.WriteLine("Điểm Tiếng Anh: " + student.EnglishScore);
+    Console.WriteLine("Điểm Khoa học: " + student.ScienceScore);
+    Console.WriteLine("Điểm Trung Bình: " + student.AverageScore());
+    Console.WriteLine("Xếp loại: " + student.GetClassification());
 }
